5ddb587b263b5600817ef5b2765bc0f264bad2fc
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-07-30 04:39:12 +0900

    Create main.java

---
+++ BackJoon/2752/main.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class back2752 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] intArr = new int[3];
+
+        intArr[0] = sc.nextInt();
+        intArr[1] = sc.nextInt();
+        intArr[2] = sc.nextInt();
+
+        Arrays.sort(intArr);
+
+        for(int i = 0; i < 3 ; i++) {
+            if(i<2)
+                System.out.print(intArr[i]+" ");
+            else
+                System.out.print(intArr[i]);
+        }
+
+    }
+}

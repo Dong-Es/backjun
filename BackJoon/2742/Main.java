135058afca05193c1324b9ed9e875af7f49b80f3
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-08-02 23:16:18 +0900

    Update Main.java

--- BackJoon/2742/Main.java
+++ BackJoon/2742/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class baek2742 {
+public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();

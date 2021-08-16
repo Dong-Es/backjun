594f610162726106492b7ba432e439f7a9506128
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-08-16 21:00:19 +0900

    Create main.py

---
+++ BackJoon/4153/main.py
@@ -0,0 +1,12 @@
+while True:
+    a=list(map(int,input().split()))
+
+    a.sort()
+
+    if a[0]==a[1]==a[2]==0:
+        break
+    a.sort()
+    if a[2]**2 == a[0]**2+a[1]**2:
+        print('right')
+    else:
+        print('wrong')

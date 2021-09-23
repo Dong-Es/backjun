0d49482257b385056a92974a7449d674080b7593
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-09-23 23:06:05 +0900

    Create Week_2(B).py

---
+++ University/Week_2(B).py
@@ -0,0 +1,7 @@
+#절대온도 입력하기
+absolute_temperature=int(input("절대온도를 입력하시오 : "))
+
+#절대온도를 섭씨로 변환하는 공식
+print(round(absolute_temperature-273.15))
+print(absolute_temperature-273.15)
+#소수점 반올림을 한 경우 round 함수를 써주면 됩니다.

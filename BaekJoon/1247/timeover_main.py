b8b1825674be42d9f1a08271e2fdbfae864dbcda
임동균 <70128434+Dong-Es@users.noreply.github.com>
2022-02-21 23:56:16 +0900

    Update timeover_main.py

--- BaekJoon/1247/timeover_main.py
+++ BaekJoon/1247/timeover_main.py
@@ -1,17 +1,29 @@
-repeat= 3
-sum_=0
-for i in range(repeat):
+for i in range(3):
     test = int(input())
-    for _ in range(test):
-        number = int(input())
-        sum_ += number
-
-    if sum_ == 0:
+    number = [int(input()) for _ in range(test)]
+    
+    if sum(number) == 0:
         print(0)
-    elif sum_ > 0:
+    elif sum(number) > 0:
         print('+')
     else:
         print('-')
     
     
 ##이런식으로 하면 시간 초과 오류가 나온다
+
+import sys
+from sys import stdin
+
+for i in range(3):
+    test = int(input())
+    number = [int(input()) for _ in range(test)]
+    
+    if sum(number) == 0:
+        print(0)
+    elif sum(number) > 0:
+        print('+')
+    else:
+        print('-')
+    
+    
